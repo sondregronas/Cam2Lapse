4b9a687d9033470ecdba5543342c2ae4e4969198
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2024-05-08 16:14:02 +0200

    Create setup-client.sh

---
+++ setup-client.sh
@@ -0,0 +1,46 @@
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get -y install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Start docker service
+systemctl enable docker
+systemctl start docker
+
+# Pull Cam2Lapse
+docker pull ghcr.io/sondregronas/cam2lapse:latest
+
+# Install Wireguard / Tailscale
+curl -fsSL https://tailscale.com/install.sh | sh
+
+# Create file
+cat << 'EOF' >> start.sh
+# NOTE: CAM set to '' will result in "latest" being used, saves in folder "1" in the receiver
+export RTSP_URL="rtsp://<CAMERA-IP>"
+export SEND_TO_RECEIVER="y"
+export CAM="CAM1"
+export URL="https://<RECEIVER-IP>"
+export TOKEN="1234567890"
+
+docker run -d \
+    -e RTSP_URL=$RTSP_URL \
+    -e SEND=$SEND_TO_RECEIVER \
+    -e CAM=$CAM \
+    -e URL=$URL \
+    -e TOKEN=$TOKEN \
+    --restart unless-stopped \
+    ghcr.io/sondregronas/cam2lapse:latest
+EOF
+
+tailscale up
